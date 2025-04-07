f322a57ca801ede5b343056cde67562d98208363
Bruno Rafael De la Garza Santos <brunorafaeldelagarza@gmail.com>
2025-04-07 15:12:40 -0600

    Se inicializaron los atributos lado y area

--- Ejemplo_evidencia3.py
+++ Ejemplo_evidencia3.py
@@ -1,2 +1,13 @@
 class Cuadrado:
-    pass
+    def __init__ (Self, Lado):
+        Self.lado = 0
+        Self.Area = 0
+    
+    def ingresar_datos (Self):
+       self.lado = float(input("Ingresa el valor del lado"))
+       
+    def Calcular_area (Self):
+        return self.Area ** 2
+    def Resultado(Self):
+        print(f"lado: {Self.lado}")
+        print(f"Area: {Self.Area}")
