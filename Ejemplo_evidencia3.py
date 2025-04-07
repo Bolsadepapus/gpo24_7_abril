35f8a3a74661fefd1400aad315ca7b0187d97251
Bruno Rafael De la Garza Santos <brunorafaeldelagarza@gmail.com>
2025-04-07 15:33:30 -0600

    Version final del ejemplo

--- Ejemplo_evidencia3.py
+++ Ejemplo_evidencia3.py
@@ -1,13 +1,11 @@
 class Cuadrado:
-    def __init__ (Self, Lado):
-        Self.lado = 0
-        Self.Area = 0
-    
-    def ingresar_datos (Self):
-       self.lado = float(input("Ingresa el valor del lado"))
+    def __init__ (self, lado):
+        self.lado = lado
+        
+    def area (self):
+       return self.lado ** 2 
        
-    def Calcular_area (Self):
-        return self.Area ** 2
-    def Resultado(Self):
-        print(f"lado: {Self.lado}")
-        print(f"Area: {Self.Area}")
+lado = int(input("Ingresa el valor del lado: "))
+
+Cuadradoo = Cuadrado(lado)
+print ("El area del Cuadrado es: ", Cuadrado.area())
